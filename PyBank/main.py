71e7ee36fd9666562996e58e4d9ec0bcb133937e
Mariia Miller <mariiamiller@aol.com>
2019-07-29 15:55:44 -0400

    Initial version

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,40 @@
+import os
+import csv
+budget_data = os.path.join('/Users','mariamiller','Documents','python-challenge','Resources','budget_data.csv')
+with open(budget_data, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",",)
+    #first_row = next(csvreader)
+    total = 0
 
+    totalmonths=0
+    greatestincrease = 0
+    greatestdecrease = 0
+    prevrow=0
+    #totalchange=0
+    #rows = list(csvreader)
+
+    for row in csvreader:
+        totalmonths += 1
+        if totalmonths == 1:
+            continue
+        current = float(row[1])
+        total = total+float(current)
+        change=current-prevrow
+        #totalchange= totalchange + change
+        if change > greatestincrease:
+            greatestincrease = change
+        if change < greatestdecrease:
+            greatestdecrease = change
+        prevrow=current
+totalmonths -= 1
+#averagechange = totalchange/totalmonths
+print(str(totalmonths),str(total),str(greatestincrease),str(greatestdecrease))
+output_file = os.path.join("./budget_final.txt")
+with open(output_file, "w") as text_file:
+    print(f"months {totalmonths}", file=text_file)
+
+        
+
+
+
+    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,20 @@
+import os
+import csv
+electiondata = os.path.join('/Users','mariamiller','Documents','python-challenge','Resources','election_data.csv')
+with open(electiondata, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader)
+    totalvotes = 0
+    candidates = []
+    votes = {}
 
+    for row in csvreader:
+        totalvotes = totalvotes+1
+        voter_id, county, candidate = row
+        if candidate not in votes.keys():
+            votes[candidate] = 0
+        votes[candidate] += 1
+
+    print(votes)
+    print(votes.keys())
+    print(votes.values())
