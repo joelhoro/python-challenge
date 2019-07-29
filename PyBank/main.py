6fdb7421fa9bc906b4f0690c38874d8e3f176fb5
Mariia Miller <mariiamiller@aol.com>
2019-07-29 17:21:15 -0400

    Improved print layout

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,7 +10,7 @@ with open(budget_data, newline="") as csvfile:
     greatestincrease = 0
     greatestdecrease = 0
     prevrow=0
-    #totalchange=0
+    totalchange=0
     #rows = list(csvreader)
 
     for row in csvreader:
@@ -20,21 +20,17 @@ with open(budget_data, newline="") as csvfile:
         current = float(row[1])
         total = total+float(current)
         change=current-prevrow
-        #totalchange= totalchange + change
+        totalchange= totalchange + change
         if change > greatestincrease:
             greatestincrease = change
+            increasemonth = row[0]
         if change < greatestdecrease:
             greatestdecrease = change
+            decreasemonth = row[0]
         prevrow=current
 totalmonths -= 1
-#averagechange = totalchange/totalmonths
-print(str(totalmonths),str(total),str(greatestincrease),str(greatestdecrease))
+averagechange = totalchange/totalmonths
+print("Total number of months: "+str(totalmonths)+ ". Average change: " +str(averagechange)+". Total Profit/Loss: " + str(total)+ ". Greatest increase in Profits: "+ str(greatestincrease)+" in "+increasemonth+". Greatest decrease in Profits: " + str(greatestdecrease)+" in "+decreasemonth)
 output_file = os.path.join("./budget_final.txt")
 with open(output_file, "w") as text_file:
-    print(f"months {totalmonths}", file=text_file)
-
-        
-
-
-
-    
+    print(f"Total number of months: {totalmonths}. Average change: {averagechange}. Total Profit/Loss: {total}. Greatest increase in Profits: {greatestincrease} in {increasemonth}. Greatest decrease in losses: {greatestdecrease} in {decreasemonth}", file=text_file)
