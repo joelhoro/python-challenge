a8b324461e8f9b4a7d41554e31b73b3a23e6e3a5
Joel <joel@horowitz.com>
2019-07-29 20:28:25 -0400

    Stylistic changes
    Get best candidates out of the final 'votes' dictionary instead of adding logic in the middle of the loop (which confuses things), by using list comprehensions (i.e. [x*x for x in [1,2,3,4] if x < 4] will return [1,4,9])
    Use the votes.items() pattern, which lets you iterate over key / value of a dictionary in one go

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,27 +1,38 @@
 import os
 import csv
 electiondata = os.path.join('/Users','mariamiller','Documents','python-challenge','Resources','election_data.csv')
+
 with open(electiondata, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    # skip first line
     next(csvreader)
     totalvotes = 0
-    candidates = []
     votes = {}
-    maxvotes = 0
     for row in csvreader:
-        totalvotes = totalvotes+1
+        totalvotes += 1
         voter_id, county, candidate = row
         if candidate not in votes.keys():
             votes[candidate] = 0
         votes[candidate] += 1
-        if votes[candidate]>maxvotes:
-            maxvotes = votes[candidate]
-            winner = candidate
 
+    # get list of candidates
+    candidates = votes.keys()
+    # get the max number of votes of all candidates
+    maxvotes = max(votes.values())
+    # note there could be more than one if they tie
+    best_candidates = [ candidate for candidate in candidates if votes[candidate] == max_votes ]
+
+    if best_candidates.length > 1:
+        ### what do you do if they tie?
+        print("....")
+    
+    winner = best_candidates[0]
     print("Total votes: "+ str(totalvotes))
     print("The winner is "+winner)
-    for candidate in votes.keys():
-        print(f"{candidate}: {votes[candidate]}, {votes[candidate]/totalvotes*100}%")
+
+    # this votes.items() is an elegant way to get both the key and the value (instead of doing votes[candidate])
+    for (candidate,votes_for_candidate) in votes.items():
+        print(f"{candidate}: {votes_for_candidate}, {votes_for_candidate/totalvotes*100}%")
 
 output_file = os.path.join("./poll_final.txt")
 with open(output_file, "w") as text_file:
